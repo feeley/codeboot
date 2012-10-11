d071d3a5763c392542c964a11365f89227637902
Bruno Dufour <dufour@iro.umontreal.ca>
2012-10-11 16:10:19 -0400

    - Cleanup comments in builtins.js

--- include/builtins.js
+++ include/builtins.js
@@ -1,20 +1,5 @@
 function print(s) {
-
     cp.addLineToTranscript(s, null);
-
-/*
-    if (cp.currentConsoleLine) {
-        $(cp.currentConsoleLine).append($("<span/>").text(s));
-    } else {
-        cp.currentConsoleLine = document.createElement('span');
-        cp.console.appendChild(cp.currentConsoleLine);
-        $(cp.currentConsoleLine).addClass("console-output").text(s);
-    }
-    
-    if (s.charAt(s.length - 1) === "\n") {
-        cp.currentConsoleLine = undefined;
-    }
-*/
 }
 
 print.toString = function () {
@@ -22,18 +7,7 @@ print.toString = function () {
 };
 
 function println(s) {
-
     cp.addLineToTranscript(s, null);
-
-/*
-    if (cp.currentConsoleLine) {
-        $(cp.currentConsoleLine).append($("<span/>").text(s));
-        cp.currentConsoleLine = undefined;
-        $(cp.console).append("\n");
-    } else {
-        cp.addLineToConsole(s, "console-output");
-    }
-*/
 }
 
 println.toString = function () {
