04d40bc2e9d033b26edd2e8ee87f802c6857bccf
Bruno Dufour <dufour@iro.umontreal.ca>
2012-10-11 16:19:39 -0400

    - Moved state serialization/deserialization actions to output.js

--- include/storage.js
+++ include/storage.js
@@ -14,3 +14,42 @@ cp.loadSession = function () {
         cp.restoreState(JSON.parse(state));
     }
 };
+
+cp.serializeState = function () {
+    var state = {
+        tabs: [],
+        repl: {
+            history: undefined
+        },
+        devMode: cp.devMode
+    };
+    
+    state.repl.history = cp.repl.cp.history.serializeState();
+    
+    return state;
+};
+
+cp.restoreState = function (state) {
+    if (state === undefined) return;
+    
+    try {        
+        // Restore history
+        cp.repl.cp.history.restoreState(state.repl.history);
+        cp.setDevMode(!!state.devMode);
+    } catch (e) {
+        cp.reportError("Unable to restore state: " + e);
+    }
+};
+
+cp.saveProject = function (cm) {
+    var state = cp.serializeState();
+    cp_internal_saveFile(JSON.stringify(state), "project.cp");
+};
+
+cp.loadProject = function (cm) {
+    cp_internal_openFile(function (file) {
+        cp_internal_readTextFile(file, function (contents) {
+            cp.restoreState(JSON.parse(contents));
+        });
+    });
+};
