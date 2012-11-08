5ca449b7820855984a7a6018be9ccc05fae7fe80
Bruno Dufour <dufour@iro.umontreal.ca>
2012-11-08 12:18:58 -0500

    Updated google API key

--- shorten.php
+++ shorten.php
@@ -8,7 +8,7 @@
     $request = '{"longUrl": "' . $url . '"}';
 
     $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, "https://www.googleapis.com/urlshortener/v1/url?key=AIzaSyCrDgOtGNcROe5LcBMOWFQ5orU65u7J82I");
+    curl_setopt($ch, CURLOPT_URL, "https://www.googleapis.com/urlshortener/v1/url?key=AIzaSyBgiTFwHKgxC9xmrVzBqG4JjG_MlT260T8");
     curl_setopt($ch, CURLOPT_HEADER, 0);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_POST, true);
