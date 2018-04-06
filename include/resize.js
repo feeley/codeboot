b4be02b3e5761db76f1f6cae040c02529e9f4ef4
sphinxplayer <sebass_menard@hotmail.com>
2018-04-06 16:46:24 -0400

    New CodeBoot CBResize resizable zone

--- include/resize.js
+++ include/resize.js
@@ -1,29 +1,39 @@
+/**
+ * Represent a resizable zone inside codeBoot interface
+ * @constructor
+ * @TODO add requestAnimationFrame to update resizing
+ */
 function CBResize () {
 
+  // Page body information
   this.body = $( 'body' );
   this.bodyRect = this.body[0].getBoundingClientRect();
 
+  // Codeboot repl container information
   this.repl = $( '#cb-repl-container' );
   this.replRect = this.repl[0].getBoundingClientRect();
   this.replStyle = window.getComputedStyle( this.repl[0] );
   this.replMargin = parseInt( this.replStyle.marginRight, 10 );
   this.replPadding = parseInt( this.replStyle.paddingRight, 10 );
 
+  // Codeboot console, editor and playground DOM
   this.console = $( '#cb-console' );
   this.editor = $( '#cb-editors' );
   this.play = $( '#cb-playground' );
 
+  // CodeBoot navbar and footer height
   this.navH = $( '#cb-navbar' ).height();
   this.footH = $( '#cb-footer' ).height();
 
+  // Division representing the resize icon
   this.resizeIcon = $( '<div>', { id: "cb-resize-icon" } );
 
+  // Resize icon styles
   this.resizeIcon.css({
     width: this.replMargin,
     height: this.replMargin,
     position: "absolute",
     borderRadius: "50%",
-    backgroundColor: "#7db6d5",
     cursor: "pointer",
     right: -this.replMargin,
     bottom: -this.replMargin
@@ -31,6 +41,7 @@ function CBResize () {
 
   this.repl.append( this.resizeIcon );
 
+  // CodeBoot resize zone information
   this.zone = {
     width: null,
     height: null,
@@ -48,34 +59,66 @@ function CBResize () {
 
 }
 
+/**
+ * Returns if playground division is visible
+ * @return {boolean}
+ */
 CBResize.prototype.playVisible = function () {
   return this.play.is( ':visible' );
 };
 
+/**
+ * Returns repl container height
+ * @return {number}
+ */
 CBResize.prototype.replHeight = function () {
   return this.repl.innerHeight();
 };
 
+/**
+ * Returns page body rect
+ * @return {DOMRect}
+ */
 CBResize.prototype.getBodyRect = function () {
   return this.bodyRect;
 };
 
+/**
+ * Returns playground width
+ * @return {number}
+ */
 CBResize.prototype.playWidth = function () {
   return this.play.width();
 };
 
+/**
+ * Returns the total margins
+ * @return {number}
+ */
 CBResize.prototype.getMargins = function () {
   return 4 * this.replMargin;
 };
 
+/**
+ * Returns the total paddings
+ * @return {number}
+ */
 CBResize.prototype.getPaddings = function () {
   return 2 * this.replPadding;
 };
 
+/**
+ * Update resize icon background color
+ * @param {string} color
+ */
 CBResize.prototype.updateColor = function ( color ) {
   this.resizeIcon.css({ backgroundColor: color });
 };
 
+/**
+ * Initialize event listeners on resize icon and page document
+ * Update resizable zone information
+ */
 CBResize.prototype.initEvents = function () {
 
   this.resizeIcon.on( 'mousedown', event => {
@@ -90,6 +133,9 @@ CBResize.prototype.initEvents = function () {
 
 };
 
+/**
+ * Update resizable zone information
+ */
 CBResize.prototype.updateZone = function ( event ) {
 
   this.bodyRect = this.body[0].getBoundingClientRect();
@@ -116,6 +162,9 @@ CBResize.prototype.updateZone = function ( event ) {
 
 };
 
+/**
+ * Update resizable zone x and y value
+ */
 CBResize.prototype.updatePosition = function ( event ) {
 
   let x = event.pageX;
@@ -143,6 +192,9 @@ CBResize.prototype.updatePosition = function ( event ) {
 
 };
 
+/**
+ * Update resizable zone elements
+ */
 CBResize.prototype.updateView = function () {
 
   this.updateRepl();
@@ -157,6 +209,9 @@ CBResize.prototype.updateView = function () {
 
 };
 
+/**
+ * Update repl container size
+ */
 CBResize.prototype.updateRepl = function () {
 
   let width = this.zone.x - this.zone.dX - this.replMargin;
@@ -166,21 +221,25 @@ CBResize.prototype.updateRepl = function () {
 
 };
 
+/**
+ * Update editor size
+ */
 CBResize.prototype.updateEditor = function () {
 
   let width = "auto";
   let height = this.zone.height - this.console.height();
 
-  console.log( this.zone.height, this.console.height() );
-
   updateCSS( this.editor, width, height );
 
 };
 
+/**
+ * Update playground size
+ */
 CBResize.prototype.updatePlay = function () {
 
-  let margins = 4 * this.replMargin;
-  let paddings = 2 * this.replPadding;
+  let margins = this.getMargins();
+  let paddings = this.getPaddings();
 
   let width = this.zone.width - this.repl.width() - margins - paddings;
   let height = this.repl.innerHeight();
@@ -191,6 +250,9 @@ CBResize.prototype.updatePlay = function () {
 
 var cbR = new CBResize();
 
+/**
+ * Resizing function, update resizable zone and view
+ */
 function startResizing ( event ) {
 
   cbR.updatePosition( event );
@@ -199,6 +261,9 @@ function startResizing ( event ) {
 
 }
 
+/**
+ * Remove event listeners on page document
+ */
 function removeEvents () {
 
   $(document).off( 'mousemove', startResizing );
@@ -207,6 +272,9 @@ function removeEvents () {
 
 }
 
+/**
+ * Update DOM element css properties width and height
+ */
 function updateCSS ( element, width, height ) {
 
   element.css({
