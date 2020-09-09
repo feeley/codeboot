4a4f907a8e4655d3611da489e29b05e59ada21bf
Olivier Melancon <ol.melancon@gmail.com>
2020-09-09 12:49:16 -0400

    rename syntax error to compilationError

--- include/lang/py/py.js
+++ include/lang/py/py.js
@@ -112,7 +112,7 @@ LangPy.prototype.compile = function (source, container, reboot) {
 
     }
 
-    function syntaxError(start_line0,
+    function compilationError(start_line0,
                          start_column0,
                          end_line0,
                          end_column0,
@@ -132,13 +132,13 @@ LangPy.prototype.compile = function (source, container, reboot) {
                                  end_line0,
                                  end_column0);
 
-        lang.vm.syntaxError(loc, error_kind + ": ", msg);
+        lang.vm.compilationError(loc, error_kind + ": ", msg);
     }
 
     var lang = this;
     var external_context =
       {
-        syntaxError: syntaxError
+        compilationError: compilationError
       };
 
     var ast = pyinterp.parse(source,
